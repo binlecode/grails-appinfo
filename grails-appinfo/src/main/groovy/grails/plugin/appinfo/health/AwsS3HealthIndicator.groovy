837d60577b6d7be1c1c99add95739980caac4413
Bin Le <bin.le.code@gmail.com>
2017-08-04 15:17:00 -0400

    enhance s3 check for multiple buckets support

--- grails-appinfo/src/main/groovy/grails/plugin/appinfo/health/AwsS3HealthIndicator.groovy
+++ grails-appinfo/src/main/groovy/grails/plugin/appinfo/health/AwsS3HealthIndicator.groovy
@@ -1,18 +1,25 @@
 package grails.plugin.appinfo.health
 
+import groovy.util.logging.Slf4j
 import org.springframework.boot.actuate.health.AbstractHealthIndicator
 import org.springframework.boot.actuate.health.Health
 
 /**
  * This S3 healthIndicator is based on Grails AWS SDK S3 module.
- * Basically it tries to fetch endpoint and bucket info from s3 for health check if bucket name is given
+ * Basically it tries to fetch endpoint and bucket info from s3 for health check if bucket(s) names are given
  * from Grails config (application.yml for example):
  * appinfo:
  *     aws:
  *         s3:
+ *             # either:
  *             bucket: {bucket-name}
+ *             # or:
+ *             buckets:
+ *                 - {bucket-name}
+ *                 - {another-bucket-name}
  *
  */
+@Slf4j
 class AwsS3HealthIndicator extends AbstractHealthIndicator {
     final def awsWebService
     final def s3
@@ -30,17 +37,40 @@ class AwsS3HealthIndicator extends AbstractHealthIndicator {
             def endpoint = s3.getEndpoint()
             builder.withDetail('endpoint', endpoint)
 
-            String bucket = s3Config.bucket
-            if (bucket) {
-                builder.withDetail('bucket', bucket)
+            List<String> bucketsToCheck = []
+
+            if (s3Config.bucket) {
+                bucketsToCheck.add(s3Config.bucket as String)
+            } else if (s3Config.buckets) {
+                bucketsToCheck.addAll(s3Config.buckets as List)
+            }
+
+            if (!bucketsToCheck) {
+                builder.withDetail('buckets', 'none')
+                log.warn "S3 check can not find any bucket configured, bucket check skipped!"
+                return
+            }
+
+            def bucketsCheck = []
+            def allCheckOk = true
+            bucketsToCheck.each { String bucket ->
+                def bucketDetails = [bucket: bucket]
 
                 if (s3.doesBucketExist(bucket)) {
-                    builder.up()
+                    bucketDetails.exists = true
                 } else {
-                    builder.down(new Exception('S3 check fail: bucket does not exist'))
+                    bucketDetails.exists = false
+                    log.warn "S3 check error: bucket ${bucket} does not exist"
+                    allCheckOk = false
                 }
+                bucketsCheck << bucketDetails
+            }
+            builder.withDetail('buckets', bucketsCheck)
+
+            if (allCheckOk) {
+                builder.up()
             } else {
-                builder.down(new Exception('S3 check fail: bucket not set'))
+                builder.down(new Exception('S3 check fail: bucket not all available'))
             }
         } catch (ex) {
             builder.down(new Exception("S3 check fail: ${ex.message ?: ex.toString()}"))
