dc35730ee11920f804d188aa2053a95c628bad63
Bin Le <bin.le.code@gmail.com>
2018-02-09 09:43:35 -0500

    Added a config key 'appinfo.health.mongodb.showPassword' : boolean to enable / disable password shadowing in the mongodb connection url if the url contains credential info. By default (if the key is not explicitly set), the showPassword = false.

--- grails-appinfo/src/main/groovy/grails/plugin/appinfo/health/MongodbHealthIndicator.groovy
+++ grails-appinfo/src/main/groovy/grails/plugin/appinfo/health/MongodbHealthIndicator.groovy
@@ -11,10 +11,12 @@ import org.springframework.boot.actuate.health.Health
 class MongodbHealthIndicator extends AbstractHealthIndicator {
     final def mongoBean
     final def mongodbConfig
+    final def healthMongodbConfig
 
-    MongodbHealthIndicator(mongo, mongodbConfig) {
+    MongodbHealthIndicator(mongo, mongodbConfig, healthMongodbConfig) {
         this.mongoBean = mongo
         this.mongodbConfig = mongodbConfig
+        this.healthMongodbConfig = healthMongodbConfig
     }
 
     @Override
@@ -23,7 +25,11 @@ class MongodbHealthIndicator extends AbstractHealthIndicator {
         try {
             def db
             if (mongodbConfig.url) {
-                builder.withDetail('url', mongodbConfig.url)
+                String mongodbUrl = mongodbConfig.url
+                if (!Boolean.parseBoolean(healthMongodbConfig?.showPassword?.toString())) {
+                    mongodbUrl = shadowUrlPassword(mongodbUrl)
+                }
+                builder.withDetail('url', mongodbUrl)
                 db = mongodbConfig.url.split('/')[-1]
             } else {
                 db = mongodbConfig.databaseName
@@ -38,6 +44,34 @@ class MongodbHealthIndicator extends AbstractHealthIndicator {
             }
         } catch (ex) {
             builder.down("MongoDB check fail: ${ex.message ?: ex.toString()}")
+        }
+    }
+
+    /**
+     * If the url contains username and password, then the password will be replaced as '<pswd>'
+     * For example if the connection url is: 'mongodb://user:user_password@192.168.0.1/mydb',
+     * then the shadowed url will be: 'mongodb://user:<pswd>@192.168.0.1/mydb'
+     *
+     * @param url
+     * @return shadowed url
+     */
+    protected String shadowUrlPassword(final String conn) {
+        //def conn = 'jdbc:oracle:thin:user:pswd@(DESCRIPTION = (LOAD_BALANCE=off)(FAILOVER=on'
+        //def m = conn =~ /^jdbc:oracle:thin:.+:.+@/
+
+        def ptrn = /^mongodb:\/\/.+:.+@/   // define pattern
+        def m = conn =~ ptrn               // define matcher
+
+        if (m.size() > 0) {  // match is found
+            def str = m[0]   // there can be only one match, get first
+            def tokens = str.split(':')    // split by ':'
+            tokens[-1] = '<pswd>@'         // replace last token, which is the password trailed by '@'
+            str = tokens.join(':')         // rejoin tokens to string
+
+            return conn.replaceFirst(/^mongodb:\/\/.+:.+@/, str)     // replace original conn with shadowed partial
+        } else {
+            // no match found, do nothing and return original
+            return conn
         }
     }
 
